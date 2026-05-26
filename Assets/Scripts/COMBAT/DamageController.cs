0b73185c6750a20d749bdb49a8d459961d0d6ed3
Dean <deancannavacciuolo352@gmail.com>
2026-05-26 11:34:34 +0200

    Combat (not Complete)

---
+++ Assets/Scripts/COMBAT/DamageController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    public float damageNumb;
+    public int  critChance;
+
+    public int critMultiplier;
+
+    public GameObject HealthHolder;
+
+    public HealthController healthController;
+
+     
+    
+    
+    void Start()
+    {
+
+        healthController = HealthHolder.GetComponent<HealthController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack()
+    {
+        healthController.healthNumb -= damageNumb;
+    }
+}
